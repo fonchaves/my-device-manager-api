81275d97c77f4e9b2faa66be82b92c26e065a428
Leandro Chaves <leandrochaves.f@gmail.com>
2022-11-04 19:13:53 -0300

    feat: add a new SpringBoot project

---
+++ src/main/java/me/leandrochaves/my_device_manager_api/MyDeviceManagerApiApplication.java
@@ -0,0 +1,13 @@
+package me.leandrochaves.my_device_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDeviceManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDeviceManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/leandrochaves/my_device_manager_api/MyDeviceManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.leandrochaves.my_device_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDeviceManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
