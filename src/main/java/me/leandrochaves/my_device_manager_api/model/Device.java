40eff41b08e2ce0f2824db2d788e02117f5c2cc5
Leandro Chaves <leandrochaves.f@gmail.com>
2022-11-04 19:47:46 -0300

    feat: add constructors, getters and setters

--- src/main/java/me/leandrochaves/my_device_manager_api/model/Device.java
+++ src/main/java/me/leandrochaves/my_device_manager_api/model/Device.java
@@ -30,5 +30,71 @@ public class Device {
   @Column(name = "updatedAt")
   @UpdateTimestamp
   private Timestamp updatedAt;
+
+  // CONSTRUCTORS
+
+  public Device() {
+    super();
+  }
+
+  public Device(String name, String brand) {
+    this.name = name;
+    this.brand = brand;
+  }
+
+  public Device(Long id, String name, String brand) {
+    this.id = id;
+    this.name = name;
+    this.brand = brand;
+  }
+
+  public Device(Long id, String name, String brand, Timestamp createdAt, Timestamp updatedAt) {
+    this.id = id;
+    this.name = name;
+    this.brand = brand;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  // GETTERS AND SETTERS
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public Timestamp getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Timestamp createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Timestamp getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Timestamp updatedAt) {
+    this.updatedAt = updatedAt;
+  }
   
 }
