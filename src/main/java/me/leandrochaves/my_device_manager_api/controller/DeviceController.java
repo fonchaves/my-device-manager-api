f348cb9a912a7ab6ee1a77d87ac3fac171c1513a
Leandro Chaves <leandrochaves.f@gmail.com>
2022-11-04 20:13:00 -0300

    feat: add DeviceController with save method

---
+++ src/main/java/me/leandrochaves/my_device_manager_api/controller/DeviceController.java
@@ -0,0 +1,24 @@
+package me.leandrochaves.my_device_manager_api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import me.leandrochaves.my_device_manager_api.model.Device;
+import me.leandrochaves.my_device_manager_api.service.DeviceService;
+
+@RestController
+@RequestMapping("/device")
+public class DeviceController {
+
+  @Autowired
+  DeviceService deviceService;
+
+  @PostMapping()
+  public Device saveDevice(@RequestBody Device device){
+    return deviceService.save(device);
+  }
+  
+}
